05046027f143dbf182127f919da1c2d761a2deab
Jala A. Gasimzada <jalaag@code.edu.az>
2022-09-25 11:29:00 +0400

    Bank

---
+++ Bank Application/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Application.Models
+{
+    public class BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool SoftDelete { get; set; }
+    }
+}
---
+++ Bank Application/Models/Branch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Application.Models
+{
+    class Branch
+    {
+    }
+}
